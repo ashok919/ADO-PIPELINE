2b36e0b54087fd96784d82e88ad6ba24f76cfd92
Ashok <135527355+ashok919@users.noreply.github.com>
2026-05-26 23:23:16 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "example1" {
   name     = "example1"
   location = "East Us"
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = "example2"
+  location = "East Us"
+}
+
+resource "azurerm_resource_group" "example3" {
+  name     = "example3"
+  location = "East Us"
+}
